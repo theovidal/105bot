789af59b83aaa04bf9cfa116867920a2ee7395f4
Exybore <exybore@becauseofprog.fr>
2020-03-21 23:28:22 +0100

    feat: add generate command

---
+++ commands/generate.rb
@@ -0,0 +1,22 @@
+module Coronagenda
+  module Commands
+    def Commands.generate(args)
+      waiter = $bot.send_message(690904527423012873, ':inbox_tray: *Génération en cours, veuillez patienter...*')
+
+      last = Models::Messages.last
+      args[0].to_i.times do |i|
+        date = last[:date] + (60 * 60 * 24 * (i + 1))
+        discord = $bot.send_message(690904527423012873, date)
+
+        Models::Messages.create do |message|
+          message.date = date
+          message.discord_id = discord.id
+        end
+      end
+
+      Commands::refresh
+
+      waiter.delete
+    end
+  end
+end
