4cce8f6e245d4fae780cd146a1f1a8493836b6f2
Exybore <exybore@becauseofprog.fr>
2020-03-21 23:27:29 +0100

    feat: add bot data

---
+++ lib/data.rb
@@ -0,0 +1,54 @@
+module Coronagenda
+  SUBJECTS = {
+    'french' => {
+      name: 'Français',
+      emoji: 'books'
+    },
+    'maths' => {
+      name: 'Mathématiques',
+      emoji: '1234'
+    },
+    'geography' => {
+      name: 'Géographie',
+      emoji: 'earth_americas'
+    }
+  }
+
+  EMOJI_DAYS = {
+    1 => ':one:',
+    2 => ':two:',
+    3 => ':three:',
+    4 => ':four:',
+    5 => ':five:',
+    6 => ':six:',
+    7 => ':seven:',
+    8 => ':eight:',
+    9 => ':nine:',
+    0 => ':zero:',
+  }
+
+  MONTHS = {
+    1 => 'janvier',
+    2 => 'février',
+    3 => 'mars',
+    4 => 'avril',
+    5 => 'mai',
+    6 => 'juin',
+    7 => 'juillet',
+    8 => 'août',
+    9 => 'septembre',
+    10 => 'octobre',
+    11 => 'novembre',
+    12 => 'décembre',
+  }
+
+  DAYS = {
+    0 => 'Dimanche',
+    1 => 'Lundi',
+    2 => 'Mardi',
+    3 => 'Mercredi',
+    4 => 'Jeudi',
+    5 => 'Vendredi',
+    6 => 'Samedi',
+  }
+end
