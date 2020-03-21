c39339f7964498cc5634850d9c6e956f9a52e5d8
Exybore <exybore@becauseofprog.fr>
2020-03-21 23:27:41 +0100

    feat: add database models

---
+++ lib/models.rb
@@ -0,0 +1,23 @@
+module Coronagenda
+  module Models
+    class Assignments < Sequel::Model
+      def self.prettify(assignment)
+        date = Time.at(assignment[:date])
+        subject = SUBJECTS[assignment[:subject]]
+        emoji = ":#{subject[:emoji]}:"
+        due = assignment.type == 'homework' ? "(rendu avant #{date.hour}h)" : "(à #{date.hour}h)"
+        output = "• #{emoji} #{subject[:name]} : #{assignment[:text]} #{due}\n"
+        output << "#{assignment[:link]}\n" if assignment[:link] != ''
+      end
+    end
+
+    class Messages < Sequel::Model
+      def self.prettify(message)
+        date = Time.at(message[:date])
+        days = date.day.digits
+        output = "**――――――――――――――――――――**\n"
+        output << "**:calendar_spiral: #{DAYS[date.wday]} #{EMOJI_DAYS[days[1]]}#{EMOJI_DAYS[days[0]]} #{MONTHS[date.month]}**\n\n"
+      end
+    end
+  end
+end
