c6e245f21303867a50dd31c8aa3f6ef6d2c8d7c7
Exybore <exybore@becauseofprog.fr>
2020-03-23 17:00:52 +0100

    feat: add remove command

--- src/commands/remove.rb
+++ src/commands/remove.rb
@@ -7,7 +7,17 @@ module Coronagenda
       USAGE = "remove <jour> <mois> <type> <numéro>"
 
       def self.exec(context, args)
+        day = args[0]
+        month = args[1]
+        type = args[2]
+        index = args[3].to_i - 1
 
+        context.send_message(':wastebasket: *Suppression du devoir, veuillez patienter...*')
+        assignments = Models::Assignments.from_day([day, month]).select{|a| a.type == type}
+
+        assignments[index].delete
+        Models::Messages.refresh(context, Models::Messages.from_day([day, month]))
+        context.send_message(':white_check_mark: *Devoir supprimé*')
       end
     end
   end
