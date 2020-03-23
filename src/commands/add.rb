fea4e2a706c5783e4385b916cb390825167f2a02
Exybore <exybore@becauseofprog.fr>
2020-03-23 17:01:21 +0100

    fix: optimize add command

--- src/commands/add.rb
+++ src/commands/add.rb
@@ -3,22 +3,31 @@ require_relative 'command'
 module Coronagenda
   module Commands
     class Add < Command
-      DESCRIPTION = "Ajouter un devoir ou un événement. S'il n'y a pas de lien, préciser `0`"
+      DESCRIPTION = "Ajouter un devoir ou un événement. S'il n'y a pas de lien ou d'heure de rendu, préciser `0` dans les champs correspondants."
       USAGE = 'add <jour> <mois> <heure> <matière> <type> <lien> <texte...>'
 
       def self.exec(context, args)
         context.send_message(":incoming_envelope: *Ajout du devoir/événement, veuillez patienter...*")
 
-        date = Time.new(2020, args[1], args[0], args[2])
+        day = args[0]
+        month = args[1]
+        hour = args[2]
+        subject = args[3]
+        type = args[4]
+        link = args[5]
+        text = args[6..].join(" ")
+
+        date = Time.new(2020, month, day, hour)
         Models::Assignments.create do |assignment|
           assignment.date = date
-          assignment.subject = args[3]
-          assignment.type = args[4]
-          assignment.link = args[5]
-          assignment.text = args[6..].join(" ")
+          assignment.to_give = hour.to_i == 0
+          assignment.subject = subject
+          assignment.type = type
+          assignment.link = link if link != '0'
+          assignment.text = text
         end
 
-        Commands::Refresh.exec(context, nil)
+        Models::Messages.refresh(context, Models::Messages.from_day([day, month]))
         context.send_message(":white_check_mark: *Devoir/Événement ajouté à l'agenda.*
           `Vous ne le voyez pas ? Essayez d'afficher davantage de jours via la commande show !`")
       end
