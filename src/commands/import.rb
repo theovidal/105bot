51a361471ed6697122af6c058aca6480a55f5854
Théo Vidal <exybore@becauseofprog.fr>
2020-05-28 21:37:14 +0200

    feat: prevent duplicates in homework import

--- src/commands/import.rb
+++ src/commands/import.rb
@@ -5,9 +5,16 @@ module HundredFive
     class Import < Command
       DESC = 'Importer les devoirs de Pronote'
       USAGE = 'import'
-      ARGS = {}
+      ARGS = {
+        acceptDuplicates: {
+          description: "Accepter les devoirs déjà inclus dans la base de données (si vous n'êtes pas sûrs, laissez la valeur par défaut)",
+          type: Integer,
+          boolean: true,
+          default: 0
+        }
+      }
 
-      def self.exec(context, _args)
+      def self.exec(context, args)
         waiter = Classes::Waiter.new(context, ":airplane_arriving: Importation des devoirs depuis Pronote, veuillez patienter...", "L'exécution de cette commande peut être plus ou moins longue, selon le nombre de devoirs à importer.")
 
         begin
@@ -16,17 +23,25 @@ module HundredFive
           raise Classes::ExecutionError.new(waiter, "une erreur s'est produite lors de l'ouverture du fichier du crawler. Vérifiez le chemin dans la configuration de 105bot.")
         end
 
+        imported = 0
+
         assignments = JSON.parse(crawler_file)
         assignments.each do |assignment|
+          p args[:acceptDuplicates]
+          p Models::Assignments.where(text: assignment['content']).first.nil?
+          next unless Models::Assignments.where(text: assignment['content']).first.nil? || args[:acceptDuplicates]
+
           Models::Assignments.create do |model|
             model.date = Time.at(assignment['due'])
             model.subject = IMPORT_SUBJECTS[assignment['subject']]
             model.type = 'homework'
             model.text = assignment['content']
           end
+          imported += 1
         end
 
-        waiter.finish("Importation des devoirs effectuée.", "Rafraichissez l'agenda pour les voir apparaître.")
+        subtext = imported < 1 ? nil : "Rafraichissez l'agenda pour les voir apparaître."
+        waiter.finish("Importation de #{imported} devoirs effectuée.", subtext)
       end
     end
   end
