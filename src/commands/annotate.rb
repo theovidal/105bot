9d9fcc9a956133b78e1c7ec4ff2c2725f0fd719f
Théo Vidal <exybore@becauseofprog.fr>
2020-06-02 20:10:04 +0200

    feat: add annotate command
    Added a command to annotate messages, with a description and even a file. Useful when students give their homework and the teacher adds comments to them

---
+++ src/commands/annotate.rb
@@ -0,0 +1,76 @@
+require_relative 'command'
+
+module HundredFive
+  module Commands
+    class Annotate < Command
+      DESC = 'Annoter une copie envoyée dans un salon textuel'
+      LISTEN = %w(private)
+      CATEGORY = 'notes'
+      ARGS = {
+        link: {
+          description: 'Lien du message : Clic droit > Copier le lien du message',
+          type: String,
+          default: nil
+        },
+        text: {
+          description: 'Texte à ajouter à la copie',
+          type: String,
+          default: nil,
+          extend: true
+        }
+      }
+
+      def self.exec(context, args)
+        url = URI(args[:link])
+        url_parts = url.path.split('/')
+
+        message_id = url_parts[-1]
+        channel_id = url_parts[-2]
+        server_id = url_parts[-3]
+
+        server = context.bot.server(server_id)
+        channel = context.bot.channel(channel_id)
+        message = channel.message(message_id)
+
+        author = server.member(message.author.id)
+        annotator = server.member(context.author.id)
+
+        source_files = ''
+        message.attachments.each do |file|
+          source_files << "#{file.filename} : #{file.url}\n"
+        end
+
+        sent_files = ''
+        context.message.attachments.each do |file|
+          sent_files << "#{file.filename} : #{file.url}\n"
+        end
+
+        fields = []
+        fields.push(Discordrb::Webhooks::EmbedField.new(
+          name: ':card_box: Fichiers source',
+          value: source_files
+        )) unless source_files == ''
+        fields.push(Discordrb::Webhooks::EmbedField.new(
+          name: ':dividers: Fichiers joints',
+          value: sent_files
+        )) unless sent_files == ''
+
+        channel.send_embed('', Utils.embed(
+          title: ":memo: Annotation de #{annotator.nick}",
+          description: args[:text],
+          author: Discordrb::Webhooks::EmbedAuthor.new(
+            icon_url: author.avatar_url,
+            name: author.nick
+          ),
+          fields: fields
+        ))
+
+        context.send_embed('', Utils.embed(
+          title: ':warning: Attention',
+          description: "Ne supprimez ni votre message, ni celui de la personne ! Si vous le faites, les fichiers ne seront plus accessibles via les liens de l'annotation.",
+          color: CONFIG['messages']['color']
+        ))
+      end
+    end
+  end
+end
