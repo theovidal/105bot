cc88344ae2bb748140a85bf7c262a38d0c3ff579
Théo Vidal <exybore@becauseofprog.fr>
2020-06-03 17:42:25 +0200

    feat: add tts for sessions

--- src/commands/session.rb
+++ src/commands/session.rb
@@ -7,19 +7,40 @@ module HundredFive
       CATEGORY = 'notes'
 
       def self.exec(context, _)
+        context.message.delete
         waiter = Classes::Waiter.new(context, ":microphone2: Session en cours par #{context.user.nick}", "Réagissez à ce message avec l'émoticone :raising_hand: pour demander la parole. Le gérant de la session peut l'arrêter en réagissant avec l'émoticone :stop_sign:.")
-        waiter.msg.pin()
         waiter.msg.react('🙋')
+        waiter.msg.react('🔊')
         waiter.msg.react('🛑')
+        waiter.msg.pin()
+
+        tts = false
         loop do
           event = context.bot.add_await!(Discordrb::Events::ReactionAddEvent)
           next unless event.channel.id == context.channel.id
-          if event.emoji.name == '🙋'
-            context.send("🙋 #{context.user.mention} Participation demandée par #{event.user.mention}")
-          elsif event.emoji.name == '🛑' && event.user.id == context.user.id
-            waiter.msg.unpin()
-            waiter.finish(":door: La session avec #{context.user.nick} est désormais terminée.")
-            break
+          is_host = event.user.id == context.user.id
+          case event.emoji.name
+          when '🙋'
+            context.send("#{context.user.mention} Participation demandée par #{event.user.mention}", tts)
+          when '🔊'
+            if is_host
+              tts = !tts
+              emoji = tts ? ':loud_sound:' : ':mute:'
+              state = tts ? 'activé' : 'désactivé'
+              context.send("#{emoji} #{context.user.mention} Son #{state} pour les demandes de participation.")
+            else
+              event.user.pm.send_message(":x: **Vous ne pouvez pas modifier le son de la session, car vous n'en êtes pas le propriétaire.**")
+            end
+          when '🛑'
+            if is_host
+              waiter.msg.unpin()
+              waiter.finish(":door: La session avec #{context.user.nick} est désormais terminée.")
+              break
+            else
+              event.user.pm.send_message(":x: **Vous ne pouvez pas fermer la session de #{context.user.nick}, car vous n'en êtes pas le propriétaire.**")
+            end
+          else
+            event.user.pm.send_message(":question: Réaction inconnue. Merci de réagir à l'aide de celles déjà disponibles.")
           end
           waiter.msg.delete_reaction(event.user, event.emoji.name)
         end
